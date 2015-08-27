6d1c46181b1a4971dce537c27d7b34bd089ca5d0
Kazi <kkahmed@wisc.edu>
2015-08-27 13:53:04 -0500

    New rational number test added

--- test_stats.py
+++ test_stats.py
@@ -1,5 +1,5 @@
 from stats import mean
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_almost_equal
 
 def testMean():
     '''*******************************************************************
@@ -11,7 +11,7 @@ def testFloatMean():
     '''*******************************************************************
     Checks the mean function using integers with float result expected
     *******************************************************************'''
-    assert_equal(mean([1,2]),1.5)
+    assert_equal(mean([0.5,2.5]),1.5)
 
 def testNegMean():
     '''*******************************************************************
@@ -24,6 +24,12 @@ def testVaryMean():
     Checks the mean function using integer lists of varying length
     *******************************************************************'''
     assert_equal(mean([2,4,6,8]),mean([1,3,7,9]))
+    
+def testInfMean():
+    '''*******************************************************************
+    Checks the mean function to 3 places for a infinitely-repeating result
+    *******************************************************************'''
+    assert_almost_equal(mean([2,3,5]),3.333,places=3)
 
 ''' # Run tests for the mean function
 testMean()
